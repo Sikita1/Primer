2047cb0ab13ea72f3279a6bdbb5050a97f5437bd
Sikita1 <ns310893@mail.ru>
2023-06-17 14:48:14 +0300

    1111

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] private Image _image;
+
     void Start()
     {
-        
+        _image = GetComponent<Image>();
     }
 }
